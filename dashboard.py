c839863d51bde86db571562dbe94f5d0595b5350
MTaufik Iqbal <110665394+MrShel00@users.noreply.github.com>
2024-02-17 23:27:08 +0700

    dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -8,7 +8,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import option_menu from streamlit_option_menu 
+from streamlit_option_menu import option_menu 
 import matplotlib.pyplot as plt
 
 
